2a526e42b22b4805224495a59f34876ce74a8eae
Pratiks-96 <pratiksavant96@gmail.com>
2026-01-31 23:25:12 +0530

    Create Home.jsx

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import Hero from '../components/Hero';
+import IndiaMap from '../components/IndiaMap';
+import BikeCards from '../components/BikeCards';
+import ChatBot from '../components/ChatBot';
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <BikeCards />
+      <IndiaMap />
+      <ChatBot />
+    </>
+  );
+}
