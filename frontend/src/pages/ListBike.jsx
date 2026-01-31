edbcb42da18f30de5bb57bef36eceb8f89425023
Pratiks-96 <pratiksavant96@gmail.com>
2026-01-31 23:25:56 +0530

    Create ListBike.jsx

---
+++ frontend/src/pages/ListBike.jsx
@@ -0,0 +1,8 @@
+export default function ListBike() {
+  return (
+    <section style={{ padding: '60px' }}>
+      <h2>List Your Bike 🏍</h2>
+      <p>Earn money by renting your bike through PS Bike Rentals.</p>
+    </section>
+  );
+}
